2fb27f518eb13222c776264cb378a802d699a69d
Davegit config --global user.name Your <delayer2208@gmail.com>
2023-07-20 16:35:56 -0700

    Finish

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,26 @@
 #include "main.h"
+
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
- *
- * Return:Always 0
- */
+ *  * print_number - prints an integer
+ *   * @n: integer to be printed
+ *    */
 void print_number(int n)
 {
-	unsigned int i = n;
+		unsigned int n1;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = -i;
-	}
+			if (n < 0)
+					{
+								n1 = -n;
+										_putchar('-');
+											} else
+													{
+																n1 = n;
+																	}
 
-	if (i / 10)
-		print_number(i / 10);
+												if (n1 / 10)
+														{
+																	print_number(n1 / 10);
+																		}
 
-	_putchar(i % 10 + '0');
+													_putchar((n1 % 10) + '0');
 }
